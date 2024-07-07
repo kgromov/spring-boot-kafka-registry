90ec59c8a0a6d55afd0f8b5d92de0c6766d64a9a
konstantin <konst.gromov@gmail.com>
2024-07-08 02:52:51 +0300

    Almost there - scheme registry is not working

--- food-service/src/main/java/org/kgromov/service/OrderListener.java
+++ food-service/src/main/java/org/kgromov/service/OrderListener.java
@@ -1,23 +1,23 @@
 package org.kgromov.service;
 
 import com.github.javafaker.Faker;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.kgromov.schema.events.OrderPlacedEvent;
-import org.slf4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class OrderListener {
-    private final static Logger log = org.slf4j.LoggerFactory.getLogger(OrderListener.class);
     private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
-    public OrderListener(KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
     @KafkaListener(topics = "order-placed", groupId = "order-service")
-    public void onOrderPlaced(OrderPlacedEvent event) {
+    public void onOrderPlaced(Message<OrderPlacedEvent> message) {
+        var event = message.getPayload();
         log.info("Received order placed event: {}", event);
 
         var dish = Faker.instance().food();
--- order-service/src/main/java/org/kgromov/service/OrderService.java
+++ order-service/src/main/java/org/kgromov/service/OrderService.java
@@ -1,18 +1,16 @@
 package org.kgromov.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.kgromov.schema.events.OrderPlacedEvent;
-import org.slf4j.Logger;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class OrderService {
-    private final static Logger log = org.slf4j.LoggerFactory.getLogger(OrderService.class);
     private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
-
-    public OrderService(KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     public void placeOrder(OrderPlacedEvent event) {
         log.info("Sending order placed event: {}", event);
